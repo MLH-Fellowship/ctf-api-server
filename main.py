e360d497766a9d6cbe55e860f4ec782bf6ca8e99
korusuke <karan.sheth@somaiya.edu>
2021-04-02 17:09:11 +0530

    Switch to environment variables

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +6,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Take my flag", "flag": "mlh{g1t_3quals_bl4ck_m4g1c}"}
+    FLAG = os.environ["FLAG"]
+    return {"message": "Take my flag", "flag": FLAG}
